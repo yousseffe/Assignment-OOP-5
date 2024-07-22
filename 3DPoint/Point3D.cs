b072f6185618fe719510ca16514385a6494f3662
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2024-07-22 15:23:23 +0300

    3DPoint

---
+++ 3DPoint/Point3D.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_OOP_5._3DPoint
+{
+    internal class Point3D : IComparable,ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point3D(int x , int y , int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public Point3D() : this(0, 0, 0){ }
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Point3D point)
+            {
+                return X == point.X && Y == point.Y && Z == point.Z;
+            }
+            return false;
+        }
+        public static bool operator ==(Point3D a, Point3D b)
+        {
+            return a.Equals(b);
+        }
+        public static bool operator !=(Point3D a, Point3D b)
+        {
+            return !a.Equals(b);
+        }
+        public int CompareTo(object? obj)
+        {
+            Point3D point = (Point3D)obj;
+            int result = this.X.CompareTo(point.X);
+            if (result == 0)
+            {
+                result = this.Y.CompareTo(point.Y);
+            }
+            if (result == 0)
+            {
+                result = this.Z.CompareTo(point.Z);
+            }
+            return result;
+        }
+
+        public object Clone()
+        {
+            return new Point3D(X, Y, Z);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,68 @@
-﻿namespace Assignment_OOP_5
+﻿using Assignment_OOP_5._3DPoint;
+
+namespace Assignment_OOP_5
 {
     internal class Program
     {
+        static bool ValidateInput(string[] input, out int x, out int y, out int z)
+        {
+            x = y = z = 0;
+            if (input.Length != 3)
+            {
+                return false;
+            }
+
+            return int.TryParse(input[0], out x) && int.TryParse(input[1], out y) && int.TryParse(input[2], out z);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 3DPoint
+            /*Console.WriteLine("Enter coordinates for point P1 (X Y Z): ");
+                var input1 = Console.ReadLine().Split();
+                if (ValidateInput(input1, out int x1, out int y1, out int z1))
+                {
+                    Point3D P1 = new Point3D(x1, y1, z1);
+
+                    Console.WriteLine("Enter coordinates for point P2 (X Y Z): ");
+                    var input2 = Console.ReadLine().Split();
+                    if (ValidateInput(input2, out int x2, out int y2, out int z2))
+                    {
+                        Point3D P2 = new Point3D(x2, y2, z2);
+
+                        Console.WriteLine(P1.ToString());
+                        Console.WriteLine(P2.ToString());
+
+                        if (P1 == P2)
+                        {
+                            Console.WriteLine("P1 is equal to P2");
+                        }
+                        else
+                        {
+                            Console.WriteLine("P1 is not equal to P2");
+                        }
+
+                        Point3D[] points = { P1, P2, new Point3D(5, 5, 5), new Point3D(1, 2, 3) };
+                        Array.Sort(points);
+
+                        Console.WriteLine("Sorted Points:");
+                        foreach (Point3D point in points)
+                        {
+                            Console.WriteLine(point.ToString());
+                        }
+
+                        Point3D P3 = (Point3D)P1.Clone();
+                        Console.WriteLine($"Cloned Point: {P3.ToString()}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input for point P2.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input for point P1.");
+                }*/
+            #endregion
         }
     }
 }
